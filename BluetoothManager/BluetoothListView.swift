d6ae5a7570ec17ae892e7bb3aa38538066b87203
Yahya Bakhtiarnia <bakhtiarniayahya1378@gmail.com>
2023-09-18 11:22:31 +0330

    first commit

---
+++ BluetoothManager/BluetoothListView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct BluetoothListView: View {
+  var body: some View {
+    VStack {
+      Text("Bluetooth Manager")
+    }
+    .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    BluetoothListView()
+  }
+}
